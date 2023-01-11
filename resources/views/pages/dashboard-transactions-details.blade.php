9da6db99ed04f3b2ea70dfa9818547c5e0965220
hrzaa <harezayoan@gmail.com>
2023-01-11 23:30:18 +0700

    add database

--- resources/views/pages/dashboard-transactions-details.blade.php
+++ resources/views/pages/dashboard-transactions-details.blade.php
@@ -38,7 +38,7 @@
                           <div class="product-subtitle">31 December 2022</div>
                         </div>
                         <div class="col-12 col-md-6">
-                          <div class="product-title">Status</div>
+                          <div class="product-title">Payment Status</div>
                           <div class="product-subtitle text-danger">Pending</div>
                         </div>
                       </div>
@@ -89,9 +89,8 @@
                           <div class="product-subtitle">Indonesia</div>
                         </div>
                         <div class="col-12 col-md-3">
-                          <div class="product-title">Status</div>
+                          <div class="product-title">Shipping Status</div>
                           <select name="status" id="status" class="form-control" v-model="status">
-                            <option value="UNPAID">Unpaid</option>
                             <option value="PENDING">Pending</option>
                             <option value="SHIPPING">Shipping</option>
                             <option value="SUCCESS">Success</option>
